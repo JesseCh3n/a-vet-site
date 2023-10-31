0301adc1f01fffc675bd2e06aca48a59d2f85c6c
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-10-31 23:08:35 +1100

    added FixieData

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/vet-site');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/vet-site');
+
+const fixieData = process.env.FIXIE_SOCKS_HOST.split(new RegExp('[/(:\\/@/]+'));
+
+mongoose.connect(process.env.DB_CONNECTION,
+    {
+      proxyUsername: fixieData[0],
+      proxyPassword: fixieData[1],
+      proxyHost: fixieData[2],
+      proxyPort: fixieData[3]
+     },
+    (error) => {
+      if(error){
+        console.log(error)
+      }
+      console.log('Connected to database')}
+)
 
 module.exports = mongoose.connection;
