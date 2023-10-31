814500c4bc7d234f2cde114743844b2353e51e30
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-10-31 22:44:03 +1100

    Mongo DB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI );
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vet-site');
 
 module.exports = mongoose.connection;
-
-// || 'mongodb://127.0.0.1:27017/vet-site'
