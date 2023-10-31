0d7a8d4013ae0789eb9b71b6dfc05ec6bcbf9841
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-10-31 23:24:57 +1100

    Mongo DB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,21 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/vet-site');
-
-const fixieData = process.env.FIXIE_SOCKS_HOST.split(new RegExp('[/(:\\/@/]+'));
-
-mongoose.connect(process.env.DB_CONNECTION,
-    {
-      proxyUsername: fixieData[0],
-      proxyPassword: fixieData[1],
-      proxyHost: fixieData[2],
-      proxyPort: fixieData[3]
-     },
-    (error) => {
-      if(error){
-        console.log(error)
-      }
-      console.log('Connected to database')}
-)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017:27017/vet-site');
 
 module.exports = mongoose.connection;
