954cd90732b0df0fad55e32855e8a142de3e1175
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-11-01 17:09:55 +1100

    Moved images to public folder

--- client/src/content_option.js
+++ client/src/content_option.js
@@ -15,7 +15,7 @@ const introdata = {
         sixth: "General Vet Care"
     },
     description: "Dedicated vets with more than 50 years of experience.",
-    your_img_url: "./src/me.jpg",
+    your_img_url: '/me.jpg',
 };
 
 const dataabout1 = {
@@ -44,63 +44,63 @@ const dataabout5 = {
 };
 
 const datavets = [{
-        img: "./src/tiger.jpg",
+        img: "/tiger.jpg",
         description: "Michael is the practice owner and Principal Veterinarian. His eagerness to serve and dedication to all animals is well known to his clients. .",
         name: "Michael",
     },
     {
-        img: "./src/cat1.jpg",
+        img: "/cat1.jpg",
         description: "Sarah grew up on a farm and has worked in small animal practices for many years and has a load of experience on varieties of medical and surgical conditions.",
         name: "Sarah",
     },
     {
-        img: "./src/cat2.jpg",
+        img: "/cat2.jpg",
         description: "Candice graduated from Melbourne University. Anna is very welcoming and holistic in her approach to providing quality care for family pets.",
         name: "Candice",
     },
     {
-        img: "./src/dogpuppy.jpg",
+        img: "/dogpuppy.jpg",
         description: "A dedicated receptionist who takes customers' need as No.1 priority.",
         name: "Cathy",
     },
     {
-        img: "./src/dog1.jpg",
+        img: "/dog1.jpg",
         description: "A vet nurse with over 10 years of experience. He has good attention to detail",
         name: "Brad",
     },
     {
-        img: "./src/dog3.jpg",
+        img: "/dog3.jpg",
         description: " She moved to Melbourne in 2020. She is a qualified Veterinary Nurse who loves surgery.",
         name: "Michelle",
     },
 
     {
-        img: "./src/cat3.jpg",
+        img: "/cat3.jpg",
         description: "She immigrated from Korea and settled down in our clinic as an energetic Veterinary Nurse.",
         name: "Felicia",
     },
     {
-        img: "./src/dog4.jpg",
+        img: "/dog4.jpg",
         description: "A part time Veterinary Nurse and a full time mum, she looks after all animals with dedications.",
         name: "Amy",
     },
     {
-        img: "./src/cat4.jpg",
+        img: "/cat4.jpg",
         description: "She is our trainee Vet, she has a passion for animals",
         name: "Brooke",
     },
     {
-        img: "./src/dog2.jpg",
+        img: "/dog2.jpg",
         description: "Retired Vet, who occationally helps us.",
         name: "Grant",
     },
     {
-        img: "./src/dog5.jpg",
+        img: "/dog5.jpg",
         description: "An experienced receptionist.",
         name: "Carla",
     },
     {
-        img: "./src/dog6.jpg",
+        img: "/dog6.jpg",
         description: "Dedicated Veterinary Nurse who goes above and beyond to help our customers.",
         name: "Ange",
     },
